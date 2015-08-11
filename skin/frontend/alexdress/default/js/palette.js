28ba5865a5af6731970ab6ea77e49d5be68b1588
onotole404 <onotole404@gmail.com>
2015-08-11 16:48:49 +0300

    palette JS correction

--- skin/frontend/alexdress/default/js/palette.js
+++ skin/frontend/alexdress/default/js/palette.js
@@ -1,5 +1,6 @@
 $j(document).ready(function(){
-	if(document.getElementById('colorPalette')){
+	var colorPalette=document.getElementById('colorPalette');
+	if(colorPalette){
 		var propSelectors={
 			'color':'a:hover, .footer-nav-title, .block-layered-nav .block-content > dl > dt, .product-view .product-shop .price-info, #configurable_swatch_size li.selected .swatch-link, #block-related .product-lnk:hover, .product-shop-title, .product-view .product-shop .product-name .h1, .product-view .product-img-box .product-name .h1, .breadcrumbs a:hover, .product-collateral .toggle-tabs li.current span, .product-collateral .toggle-tabs li:hover span, .product-view .product-shop #block-related .product-lnk:hover .price-box .regular-price .price, .product-view .product-shop #block-related .product-lnk:hover .price-box .special-price .price, .product-view .product-shop #block-related .product-lnk:hover .price-box .old-price .price, .header-minicart .subtotal .price, .skip-cart',
 			'background-color':'#header-nav, #search_mini_form .search-button, .lang-current::before, .home-widget-overlay, .footer-copyright, .footer-extra .block-content .actions .button, .products-item:hover::after, .button, .cart-table .product-cart-actions .button, #co-shipping-method-form .buttons-set .button, .footer .button, #block-related .product-lnk:hover::after',
@@ -9,11 +10,11 @@ $j(document).ready(function(){
         	'border-bottom-color':'.product-shop-title'
         /*'border-left-color':''*/
 		};
-		activatePalette(document.getElementById('palette-color'),document.getElementById('palette-control'),document.getElementById('palette-reset'),propSelectors);
+		activatePalette(colorPalette,document.getElementById('palette-color'),document.getElementById('palette-control'),document.getElementById('palette-reset'),propSelectors);
 	}
 });
 
-function activatePalette(colorInput,colorControl,colorReset,propSelectors){
+function activatePalette(colorPalette,colorInput,colorControl,colorReset,propSelectors){
     var clCustom=window.localStorage['clCustom'],clDefault=colorInput.value;
 	if(clCustom){
         applyCl(colorInput.value=clCustom,propSelectors);
