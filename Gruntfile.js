b934585fba8d0625a2dfd09b2c13b0f4f2d6b737
onotole404 <onotole404@gmail.com>
2016-02-01 01:25:01 +0300

    Grunt watch setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,28 @@
 'use strict';
 
 module.exports = function (grunt) {
-	grunt.initConfig({
-    compass: {
-      compile: {
-        options: {
-          sassDir: 'skin/frontend/alexdress/default/scss',
-          cssDir: 'skin/frontend/alexdress/default/css',
-					imagesDir: 'skin/frontend/alexdress/default/images',
-					specify: 'skin/frontend/alexdress/default/scss/*.scss'
+    grunt.initConfig({
+        compass: {
+            compile: {
+                options: {
+                    sassDir: 'skin/frontend/alexdress/default/scss',
+                    cssDir: 'skin/frontend/alexdress/default/css',
+                    imagesDir: 'skin/frontend/alexdress/default/images',
+                    specify: 'skin/frontend/alexdress/default/scss/*.scss'
+                }
+            }
+        },
+        watch: {
+            scss: {
+                files: ['skin/frontend/alexdress/default/scss/**/*.scss'],
+                tasks: ['compass'],
+                options: {
+                    spawn: false
+                }
+            }
         }
-      }
-		}
-	});
-  grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.registerTask('default',['compass']);
+    });
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['compass']);
 };
