3e30457f173bbd6f674047295a2e4f0e6ab332f0
onotole404 <onotole404@gmail.com>
2016-02-06 20:10:36 +0300

    AlexStuff theme init

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,22 @@
 'use strict';
 
+var storeName='alexstuff';
+
 module.exports = function (grunt) {
     grunt.initConfig({
         compass: {
             compile: {
                 options: {
-                    sassDir: 'skin/frontend/alexdress/default/scss',
-                    cssDir: 'skin/frontend/alexdress/default/css',
-                    imagesDir: 'skin/frontend/alexdress/default/images',
-                    specify: 'skin/frontend/alexdress/default/scss/*.scss'
+                    sassDir: 'skin/frontend/'+storeName+'/default/scss',
+                    cssDir: 'skin/frontend/'+storeName+'/default/css',
+                    imagesDir: 'skin/frontend/'+storeName+'/default/images',
+                    specify: 'skin/frontend/'+storeName+'/default/scss/*.scss'
                 }
             }
         },
         watch: {
             scss: {
-                files: ['skin/frontend/alexdress/default/scss/**/*.scss'],
+                files: ['skin/frontend/'+storeName+'/default/scss/**/*.scss'],
                 tasks: ['compass'],
                 options: {
                     spawn: false
---
+++ skin/frontend/alexstuff/default/js/palette.js
@@ -0,0 +1,50 @@
+$j(document).ready(function(){
+	var colorPalette=document.getElementById('colorPalette');
+	if(colorPalette){
+		var propSelectors={
+			'color':'a:hover, .footer-nav-title, .block-layered-nav .block-content > dl > dt, .product-view .product-shop .price-info, #configurable_swatch_size li.selected .swatch-link, #block-related .product-lnk:hover, .product-shop-title, .product-view .product-shop .product-name .h1, .product-view .product-img-box .product-name .h1, .breadcrumbs a:hover, .product-collateral .toggle-tabs li.current span, .product-collateral .toggle-tabs li:hover span, .product-view .product-shop #block-related .product-lnk:hover .price-box .regular-price .price, .product-view .product-shop #block-related .product-lnk:hover .price-box .special-price .price, .product-view .product-shop #block-related .product-lnk:hover .price-box .old-price .price, .header-minicart .subtotal .price, .skip-cart',
+			'background-color':'#header-nav, #search_mini_form .search-button, .lang-current::before, .home-widget-overlay, .footer-copyright, .footer-extra .block-content .actions .button, .products-item:hover::after, .button, .cart-table .product-cart-actions .button, #co-shipping-method-form .buttons-set .button, .footer .button, #block-related .product-lnk:hover::after, .std-btn, .socialIcon:hover',
+			'border-color':'.skip-cart .icon, #header-cart',
+	        'border-top-color':'.nav-primary li.level0.parent.menu-active::after',
+/*        'border-right-color':'',*/
+        	'border-bottom-color':'.product-shop-title'
+        /*'border-left-color':''*/
+		};
+		activatePalette(colorPalette,document.getElementById('palette-color'),document.getElementById('palette-control'),document.getElementById('palette-reset'),propSelectors);
+	}
+});
+
+function activatePalette(colorPalette,colorInput,colorControl,colorReset,propSelectors){
+    var clCustom=window.localStorage['clCustom'],clDefault=colorInput.value,clPrimary=colorInput.getAttribute('data-clprimary');
+	if(clCustom){
+        applyCl(colorInput.value=clCustom,propSelectors);
+	}
+	else{
+		if(clDefault!=clPrimary){
+    	    applyCl(clDefault,propSelectors);
+		}
+	}
+    colorControl.on('click',function(){
+       colorPalette.classList.toggle('palette-active');
+       this.classList.toggle('fa-paint-brush');
+       this.classList.toggle('fa-times');
+    });
+    colorInput.on('input',function(){
+       applyCl(window.localStorage['clCustom']=this.value,propSelectors);
+    });
+    colorReset.on('click',function(){
+       applyCl(colorInput.value=clDefault,propSelectors);
+       window.localStorage['clCustom']='';
+    });
+	function applyCl(cl,propSelectors){
+		var customStylesheet=document.getElementById('customClRules');
+		if(customStylesheet){
+            customStylesheet.remove();
+		}
+		var styleStr='<style id="customClRules">';
+		for(var prop in propSelectors){
+			styleStr+=propSelectors[prop]+'{'+prop+':'+cl+'}';
+		}
+		document.head.insert(styleStr+'</style>');
+	}
+}
