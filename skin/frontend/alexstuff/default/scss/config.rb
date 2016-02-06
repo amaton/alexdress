34e508d71f2948fef6ddeb98bc6935be238d8915
onotole404 <onotole404@gmail.com>
2016-02-07 01:53:01 +0300

    Fonts setup

--- skin/frontend/alexstuff/default/scss/config.rb
+++ skin/frontend/alexstuff/default/scss/config.rb
@@ -1,5 +1,5 @@
 # note: this should never truly be refernced since we are using relative assets
-http_path = "/skin/frontend/alexdress/default/"
+http_path = "/skin/frontend/alexstuff/default/"
 css_dir = "../css"
 sass_dir = "../scss"
 images_dir = "../images"
