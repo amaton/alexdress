c314d72cc2e5a9d45c61bdace8c23970646b9b23
pticakuk <kukushkindmitriy@yandex.ru>
2016-03-24 08:59:54 +0200

    Create home page carucels;

---
+++ app/code/local/Aldress/Page/sql/aldress_page_setup/upgrade-0.1.1-0.1.2.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * User: Ilya Voinov
+ * Email: ilya.voinov@yahoo.com
+ * Date: 12/17/13
+ */
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$cmsHomePage = Mage::getModel('cms/page')->load('home');
+if(!$cmsHomePage->getPageId()) {
+    $cmsHomePage = Mage::getModel('cms/page')->setIdentifier('home');
+}
+$layoutUpdateXml = <<<XML
+<reference name="head">
+            <action method="addItem"><type>skin_js</type><name>js/slick.min.js</name><param>async</param></action>
+            <action method="addItem"><type>skin_css</type><name>css/slick.css</name></action>
+</reference>
+<reference name="content">
+<block type="page/html_wrapper"  before="content" name="home.wrapper" >
+    <action method="setElementTagName"><tag>div</tag></action>
+    <action method="setElementClass"><class>std</class></action>
+    <block type="aldress_page/html_homepage_banners" name="homepage.banner.rotator" template="page/html/homepage/main_banners.phtml">
+        <action method="setBannerGroupId"><banner_group_id>home_banner</banner_group_id></action>
+    </block>
+    <block type="aldress_page/html_homepage_banners" name="categories.banner.rotator" template="page/html/homepage/categories_banners.phtml">
+        <action method="setBannerGroupId"><banner_group_id>categories_banner_rotator</banner_group_id></action>
+    </block>
+    <block type="aldress_page/html_homepage_banners" name="styles.banner.rotator" template="page/html/homepage/styles_banners.phtml">
+        <action method="setBannerGroupId"><banner_group_id>styles_banner_rotator</banner_group_id></action>
+    </block>
+</block>
+</reference>
+XML;
+
+$cmsHomePage->setTitle('Home page')
+    ->setContent('<div class="home-content smallContainer">Alex Dress Main Description And Meta Infarmation</div>')
+    ->setRootTemplate('one_column')
+    ->setLayoutUpdateXml($layoutUpdateXml)
+    ->setStoreId(array(0))
+    ->setActive(1);
+
+$cmsHomePage->save();
+
+$installer->endSetup();
--- skin/frontend/alexdress/default/js/slideshow.js
+++ skin/frontend/alexdress/default/js/slideshow.js
@@ -59,5 +59,50 @@ $j(document).ready(function () {
 			document.getElementById('related-container').classList.add('noCarousel');
 		}
 	}
+    $j('div.home-widgets-top').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true
+
+    });
+    $j('div.home-widgets-btm').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true
+    });
+    var img_ht=$j('div.home-widgets-top').height();
+    var img_wt=$j('div.home-widgets-top img').width();
+    $j('div.home-widgets-top img').height(img_ht);
+    var img_hb=$j('div.home-widgets-btm').height();
+    var img_wb=$j('div.home-widgets-btm img').width();
+    $j('div.home-widgets-btm img').height(img_hb);
+    /*$j( window ).resize(function() {
+        var img_wtr=$j('div.home-widgets-top img').width();
+        $j('div.home-widgets-top img').height(img_ht*img_wtr/img_wt);
+        var img_wbr=$j('div.home-widgets-btm img').width();
+        $j('div.home-widgets-btm img').height(img_hb*img_wbr/img_wb);
+    });*/
+    var resize = function(e){
+        var img_wtr=$j('div.home-widgets-top img').width();
+        $j('div.home-widgets-top img').height(img_ht*img_wtr/img_wt);
+        var img_wbr=$j('div.home-widgets-btm img').width();
+        $j('div.home-widgets-btm img').height(img_hb*img_wbr/img_wb);
+    };
+    (function(){
+        var time;
+        window.onresize = function(e){
+            if (time)
+            clearTimeout(time);
+            time = setTimeout(function(){
+                resize(e);
+            },100);
+        }
+    })();
+
+
+
+
 
 });
